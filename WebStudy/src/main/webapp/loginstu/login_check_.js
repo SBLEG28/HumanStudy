df934c1e8aa7f02168f112d63c111d3f3c24178d
soobi <soobi@DESKTOP-893TCEN>
2021-12-28 16:27:24 +0900

    return change

--- WebStudy/src/main/webapp/loginstu/login_check_.js
+++ WebStudy/src/main/webapp/loginstu/login_check_.js
@@ -8,24 +8,24 @@ $(document)
 	if (NullCheck(id)) {
 		alert("아이디를 입력하지 않았습니다.");
 		history.back();
-		return;
+		return false;
 	} else {
 		if (id != "123") {
 			alert("해당 아이디가 존재하지 않습니다.");
 			history.back();
-			return;
+			return false;
 		}
 	}
 
 	if (NullCheck(pw)) {
 		alert("비밀번호를 입력하지 않았습니다.");
 		history.back();
-		return;
+		return false;
 	} else {
 		if (pw != "123") {
 			alert("비밀번호가 틀립니다.");
 			history.back();
-			return;
+			return false;
 		}
 		else {
 			document.location = "home.jsp";
--- WebStudy/src/main/webapp/loginstu/sign_check.js
+++ WebStudy/src/main/webapp/loginstu/sign_check.js
@@ -12,43 +12,43 @@ $(document)
 	   	if (NullCheck(name)) {
 			alert("실명을 입력하지 않았습니다.");
 			history.back();
-			return;
+			return false;
 		}
 
 		if (NullCheck(gender)) {
 			alert("성별을 입력하지 않았습니다.");
 			history.back();
-			return;
+			return false;
 		}
 	
 		if (NullCheck(id)) {
 			alert("아이디를 입력하지 않았습니다.");
 			history.back();
-			return;
+			return false;
 		}
 
 		if (NullCheck(psw1)) {
 			alert("비밀번호를 입력하지 않았습니다.");
 			history.back();
-			return;
+			return false;
 		}
 
 		if (NullCheck(psw2)) {
 			alert("비밀번호 확인을 입력하지 않았습니다.");
 			history.back();
-			return;
+			return false;
 		}
 
 		if (NullCheck(inter)) {
 			alert("관심분야를 한개 이상 선택하지 않았습니다.");
 			history.back();
-			return;
+			return false;
 		}
 		
 		if ((!NullCheck(psw2)) && (!psw1.equals(psw2))) {
 			alert("비밀번호와 비밀번호 확인이 일치하지 않습니다.");
 			history.back();
-			return;
+			return false;
 		} else {
 			document.location = "login.jsp";
 		}
